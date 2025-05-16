83c704ea2a4eaa403ccf47239a9d95e00e718cb1
YeMuLong <apxiaoyou14@outlook.com>
2025-05-16 17:14:29 +0800

    demo2.c

---
+++ day25.4.24/demo2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i = 0,o = 0;
+   for(i = 1; i<=10; i++){
+    for (o = 0;o <= 10;o++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    }
+    return 0;
+}
